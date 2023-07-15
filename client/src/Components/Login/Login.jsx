f4171b4478945a954a329fe10912faf4751def4a
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-15 18:56:52 +0530

    login page made

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./Login.css"
 
 const Login = () => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
   return (
     <div id="login" className='flex-col'>
-      <h1>Login</h1>
-      <form className='signup-form' method="post" action='/login'>
-
-        <div className='subform'>
-          <label htmlFor="name">Name: </label>
-          <input type="text" name='name' placeholder='Your Name' />
-        </div>
+      <h1>login</h1>
+      <form className='login-form' method="post" action='/login' >
 
         <div className='subform'>
           <label htmlFor="email">Email: </label>
-          <input type="text" name='email' placeholder='Your Email' />
+          <input valeu={email} onChange={(e) => setEmail(e.target.value)} type="text" name='email' placeholder='Your Email' />
         </div>
 
         <div className='subform'>
           <label htmlFor="password">Password: </label>
-          <input type="text" name='password' placeholder='Your Password' />
+          <input value={password} onC hange={(e) => setPassword(e.target.value)} type="text" name='password' placeholder='Your Password' />
         </div>
 
+        <button type="submit" id="test" onClick={async (e) => {
+          e.preventDefault()
+          const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            body: JSON.stringify({
+              email: email,
+              password: password,
+            })
+          })
+
+          const json = response.json()
+          console.log(json)
+          alert("hello ther")
+        }}>SUBMIT</button>
+
       </form>
-    </div>
+    </div >
   )
 }
 
