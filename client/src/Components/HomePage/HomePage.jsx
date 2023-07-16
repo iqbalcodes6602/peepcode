078898974828a3263ea3ba62f40ebf66436ba442
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-16 19:16:02 +0530

    added functionality

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -6,14 +6,14 @@ const HomePage = () => {
   return (
     <div id="home">
       <h1 className='flex-row'>Blogs</h1>
-      {loremContent.map((content,index) => (
+      {loremContent.map((content, index) => (
         <div key={`blog-${index}`} className="blog-box">
           <p className="date">{content.date}</p>
           <h4 className='title'>{content.title}</h4>
           <p className="content">{content.content}</p>
         </div>
       ))}
-      
+
     </div>
   )
 }
