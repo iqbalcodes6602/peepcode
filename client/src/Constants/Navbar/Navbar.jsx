a0ab26816c8feb200ca6c6b4098cea82f7a9d47b
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-20 23:39:24 +0530

    otpmzn

--- client/src/Constants/Navbar/Navbar.jsx
+++ client/src/Constants/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   return (
     <div id='navbar-main' className='flex-row'>
       <Link to={'/'}>
-        <div className="logo-box flex-row"> 
-          <img className='logo' src="https://user-images.githubusercontent.com/63964149/152531278-5e01909d-0c2e-412a-8acc-4a06863c244d.png" alt="logo" /> 
+        <div className="logo-box flex-row">
+          <img className='logo' src="https://user-images.githubusercontent.com/63964149/152531278-5e01909d-0c2e-412a-8acc-4a06863c244d.png" alt="logo" />
           <p>LeetCode</p>
         </div>
       </Link>
